d438b2fe15f54e3ad9fe306c2d1e3210d2a5b59d
andreamariejimenez1 <andrea.jimenez@my.utsa.edu>
2022-10-21 10:24:49 -0500

    Added a constants file to help with organization

---
+++ SimpleThreads/Model/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  SimpleThreads
+//
+//  Created by Andrea Jimenez on 10/21/22.
+//
+
+import Foundation
+
+/*
+ This file is for all of the string constants that will be used in the app. Using a struct to store string constants means changes will only need to happend in a single file.
+ 
+ */
+
+struct AppConstants {
+    // We will use the static keyword to create type variables instead of instance variables.
+    
+    /// The cell identifier to be used in the collection view protocol methods.
+    static let collectionCellIdentifier = "sample string"
+}
+
