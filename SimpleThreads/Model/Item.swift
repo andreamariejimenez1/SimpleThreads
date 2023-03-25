3b8129e7b20e736531c0f57b0e3222ac9030e04c
BlaineBeltran <bbeltran1998@yahoo.com>
2023-03-25 16:48:57 -0500

    Updated Item Model

--- SimpleThreads/Model/Item.swift
+++ SimpleThreads/Model/Item.swift
@@ -5,14 +5,36 @@
 //  Created by Ahmet Bilici on 10/15/22.
 //
 
+import UIKit
 
-import Foundation
+enum Size: String {
+    case xSmall = "XS"
+    case small = "S"
+    case medium = "M"
+    case large = "L"
+    case xLarge = "XL"
+    case xxLarge = "XXL"
+}
 
+enum Price: String {
+    case shirts = "$15.00"
+    case hoodies = "$35.00"
+    case sweatShirts = "$25.00"
+    case toteBags = "$12.00"
+    case socks = "$8.00"
+}
 
 struct Item {
-    let itemImage: String
-    let itemName: String
-    let itemPrice: Double
-    let itemQuantity: Int
+    var name: String
+    var price: Price
+    var color: String
+    var size: Size
+    var inStock: Bool = true
+    var description: String?
+    var image: UIImage
+    
+    static let sampleData = [Item(name: "Shirt", price: .shirts, color: "Gray", size: .small, image: UIImage(named: "lightGrayShirt")!)]
 }
+
+
 
